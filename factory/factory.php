9dbc846c6fc295ef1fb8912d6ffa50f443bc9a78
INTERN TWO <Interntwo@JGichane.local>
2016-03-21 13:42:06 +0300

    Initial commit- Design patterns

---
+++ factory/factory.php
@@ -0,0 +1,30 @@
+<?php
+class Television
+{
+    private $tvMake;
+    private $tvModel;
+
+    public function __construct($make, $model)
+    {
+        $this->tvMake = $make;
+        $this->tvModel = $model;
+    }
+
+    public function getMakeAndModel()
+    {
+        return $this->tvMake . ' ' . $this->tvModel;
+    }
+}
+
+class TelevisionFactory
+{
+    public static function create($make, $model)
+    {
+        return new Automobile($make, $model);
+    }
+}
+
+// have the factory create the Television object
+$samsung = TelevisionFactory::create('samsung', 'D500X');
+
+print_r($samsung->getMakeAndModel()); // outputs "samsung D500X"
---
+++ factory/index.php
@@ -0,0 +1,3 @@
+<?php
+echo " Design patterns in php";
+?>
